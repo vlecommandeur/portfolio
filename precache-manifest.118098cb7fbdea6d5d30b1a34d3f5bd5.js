c7dc5ee415c999f08be6b3512fa474b8278e6811
vlecommandeur <valentin.lecommandeur@gmail.com>
2020-04-24 19:58:22 +0200

    Updates

---
+++ precache-manifest.118098cb7fbdea6d5d30b1a34d3f5bd5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6760ebc9e80d1894cd7990a8b619c4a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5bf61f56ebf45295f736",
+    "url": "/portfolio/static/css/main.e7008b03.chunk.css"
+  },
+  {
+    "revision": "f6824bda9115db41e0f2",
+    "url": "/portfolio/static/js/2.09491cc1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.09491cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf61f56ebf45295f736",
+    "url": "/portfolio/static/js/main.22919ff3.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "8f432cf8e2347248deece9aa423f3773",
+    "url": "/portfolio/static/media/androidImg.8f432cf8.jpg"
+  },
+  {
+    "revision": "cd318bc618bb105bf45aa2a0e4dedffc",
+    "url": "/portfolio/static/media/cooptationImg.cd318bc6.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9e7b0c6949b109551fdf5a4f8a236bb4",
+    "url": "/portfolio/static/media/luteceImg.9e7b0c69.PNG"
+  },
+  {
+    "revision": "760d4999512a5f3162e4d97ee4fcaf11",
+    "url": "/portfolio/static/media/pacmanImg.760d4999.jpg"
+  },
+  {
+    "revision": "fc1cd314137a0c5c560c6b4e5fdbeb07",
+    "url": "/portfolio/static/media/profile.fc1cd314.png"
+  },
+  {
+    "revision": "3edc44620a0cea268d4c2bee0dbd4bff",
+    "url": "/portfolio/static/media/space.3edc4462.jpg"
+  }
+]);
